e43219bc31baaedb6a1b4d5bb0f2ac7664cbb0da
kurobii528@gmail.com <kurobii528@gmail.com>
2022-10-26 10:33:17 +0900

    計算

--- 体積と表面積/体積と表面積.cpp
+++ 体積と表面積/体積と表面積.cpp
@@ -11,9 +11,9 @@ int main()
     cin >> height;
     cout << "奥行を入力:";
     cin >> depth;
-    double volume,surfase;
-    volume = getVolumeSurface(wide,height,depth,&surfase);
-    cout << "体積は" << volume << endl << "表面積は" << surfase << endl;
+    double volume,surface;
+    volume = getVolumeSurface(wide,height,depth,&surface);
+    cout << "体積は" << volume << endl << "表面積は" << surface << endl;
     return 0;
 
 }
